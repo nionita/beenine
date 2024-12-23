a8ec7251a0937bf42be10befa65cc98d4a28afd7
Nicu Ionita <nicu@ionita.at>
2024-12-23 22:10:47 +0100

    Rewrite current functionality with a python generator

--- manageds.py
+++ manageds.py
@@ -16,35 +16,9 @@ skip_chars   = 'kK/12345678'
 piece_chars  = white_pieces + black_pieces
 
 '''
-Process one EPD file and count or sample the combinations
+Process one EPD line and return the tuples for the moving and passive parts (P, R, N, B, Q)
 '''
-def proc_file(ffn, counts=None, part=None, samples=None, number=0):
-    n = 0
-    with open(ffn, 'r') as f:
-        for line in f:
-            line = line[:-1]
-            n += 1
-            #print(line)
-            me, yo = proc_line(line)
-            #print(f'{me} - {yo}')
-            if me:
-                code = f'{me}-{yo}'
-                if counts is not None:
-                    if code in counts:
-                        counts[code] += 1
-                    else:
-                        counts[code] = 1
-                if samples is not None and part is not None:
-                    if me == part or yo == part:
-                        samples.append((code, line))
-                        if number > 0 and len(samples) >= number:
-                            break
-    return n
-
-'''
-Process one EPD line and return the codes for the moving and non-moving parts
-'''
-def proc_line(line):
+def one_line(line):
     pos = { 'P': 0, 'p': 0, 'R': 0, 'r': 0, 'n': 0, 'N': 0, 'b': 0, 'B': 0, 'q': 0, 'Q': 0 }
     spaces = 0
     moving = None
@@ -59,14 +33,44 @@ def proc_line(line):
                 spaces = 1
             else:
                 break
-    code_white = f'{pos["P"]}{pos["R"]}{pos["N"]}{pos["B"]}{pos["Q"]}'
-    code_black = f'{pos["p"]}{pos["r"]}{pos["n"]}{pos["b"]}{pos["q"]}'
+    tuple_white = (pos["P"], pos["R"], pos["N"], pos["B"], pos["Q"])
+    tuple_black = (pos["p"], pos["r"], pos["n"], pos["b"], pos["q"])
     if moving == 'w':
-        return code_white, code_black
+        return tuple_white, tuple_black
     elif moving == 'b':
-        return code_black, code_white
+        return tuple_black, tuple_white
     else:
-        return '', ''
+        return (), ()
+
+'''
+Generator delivers one line at a time with further information, all in a dictionary:
+record number, file number, file name, line number, line, tuple moving part, tuple passive part
+'''
+def data_generator(root_dir, glob_pat='*.epd'):
+    rec_no  = 0
+    file_no = 0
+    for fn in glob.glob(glob_pat, root_dir=root_dir):
+        file_no += 1
+        ffn = os.path.join(root_dir, fn)
+        with open(ffn, 'r') as f:
+            line_no = 0
+            for line in f:
+                rec_no += 1
+                line_no += 1
+                line = line[:-1]
+                tmp, tpp = one_line(line)
+                yield { 'rec_no': rec_no, 'file_no': file_no, 'file_name': ffn,
+                        'line_no': line_no, 'line': line, 'moving_part': tmp, 'passive_part': tpp }
+
+'''
+Make a code from the tuple
+'''
+def tuple_to_code(tup):
+    if len(tup) != 5:
+        return 'XXXXX'
+    else:
+        p, r, n, b, q = tup
+        return f'{p}{r}{n}{b}{q}'
 
 '''
 Statistics over all files
@@ -74,17 +78,30 @@ Statistics over all files
 def do_stats(args):
     print(f'Statistics over files in {args.dir}:')
     counts = {}
-    total = 0
-    for fn in glob.glob('*.epd', root_dir=args.dir):
-        ffn = os.path.join(args.dir, fn)
-        lines = proc_file(ffn, counts)
-        print(f'{ffn}: {lines} lines')
-        total += lines
+    for line_info in data_generator(root_dir=args.dir):
+        mp = tuple_to_code(line_info['moving_part'])
+        pp = tuple_to_code(line_info['passive_part'])
+        code = f'{mp}-{pp}'
+        if code in counts:
+            counts[code] += 1
+        else:
+            counts[code] = 1
+
+        # Report progress
+        rn = line_info['rec_no']
+        if args.progress > 0 and rn % args.progress == 0:
+            fn = line_info['file_name']
+            ln = line_info['line_no']
+            print(f'Record {rn}, file {fn}, line {ln}')
+
+        # Limit the input
+        if args.limit > 0 and rn >= args.limit:
+            break
 
     with open(args.out, 'w') as f:
         for code in sorted(counts.keys()):
             print(f'{code}: {counts[code]}', file=f)
-    print(f'Total: {total} positions with {len(counts.keys())} keys')
+    print(f'Total: {rn} positions with {len(counts.keys())} keys')
 
 '''
 Samples over all files
@@ -92,10 +109,24 @@ Samples over all files
 def do_sample(args):
     print(f'Samples from files in {args.dir}:')
     samples = []
-    for fn in glob.glob('*.epd', root_dir=args.dir):
-        ffn = os.path.join(args.dir, fn)
-        lines = proc_file(ffn, part=args.side, samples=samples, number=args.number)
-        if args.number > 0 and len(samples) >= args.number:
+    for line_info in data_generator(root_dir=args.dir):
+        mp = tuple_to_code(line_info['moving_part'])
+        pp = tuple_to_code(line_info['passive_part'])
+        if mp == args.side or pp == args.side:
+            code = f'{mp}-{pp}'
+            samples.append((code, line_info['line']))
+            if args.number > 0 and len(samples) >= args.number:
+                break
+
+        # Report progress
+        rn = line_info['rec_no']
+        if args.progress > 0 and rn % args.progress == 0:
+            fn = line_info['file_name']
+            ln = line_info['line_no']
+            print(f'Record {rn}, file {fn}, line {ln}')
+
+        # Limit the input
+        if args.limit > 0 and rn >= args.limit:
             break
 
     with open(args.out, 'w') as f:
@@ -104,17 +135,17 @@ def do_sample(args):
 
 # Parse command line arguments
 parser = argparse.ArgumentParser(
-            prog='distib',
-            description='Count position distribution in epd files by piece types')
+            prog='manageds',
+            description='Count / sample / filter EPD lines in epd files by piece types')
 #parser.add_argument('--debug', action='store_true', help='enable debug')
+parser.add_argument('-d', '--dir', required=True, help='directory with EPD files')
+parser.add_argument('-o', '--out', default=sys.stdout, help='output file name')
+parser.add_argument('-p', '--progress', type=int, default=0, help='report progress')
+parser.add_argument('-l', '--limit', type=int, default=0, help='limit input')
 subparsers = parser.add_subparsers(title='subcommands', required=True)
 parser_stats = subparsers.add_parser('stats')
-parser_stats.add_argument('-d', '--dir', required=True, help='directory with EPD files')
-parser_stats.add_argument('-o', '--out', default=sys.stdout, help='output file name')
 parser_stats.set_defaults(func=do_stats)
 parser_sample = subparsers.add_parser('sample')
-parser_sample.add_argument('-d', '--dir', required=True, help='directory with EPD files')
-parser_sample.add_argument('-o', '--out', default=sys.stdout, help='output file name')
 parser_sample.add_argument('-s', '--side', required=True, help='side sample to search for')
 parser_sample.add_argument('-n', '--number', type=int, default=0, help='number of samples (default: all)')
 parser_sample.set_defaults(func=do_sample)
